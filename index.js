06cff737c3b95f0192069208a104e7c55b916c2c
Ajmol Uddin <uddin.ajmol@gmail.com>
2021-07-16 13:58:26 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, text) {
+  const newArr = [];
+  for (let name of names) {
+    newArr.push(`Thank you, ${name}, for the wonderful ${text} gift!`);
+  }
+  return newArr;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
